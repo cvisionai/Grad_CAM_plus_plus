687b609ddfe3882b8ad30993fbce9be2051b57b2
Benjamin Woodward <benjamin.woodward@cvisionconsulting.com>
2018-05-29 10:21:41 -0400

    Works with TF 1.8 and Python 3

--- misc/utils.py
+++ misc/utils.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-import GuideReLU as GReLU
+import misc.GuideReLU as GReLU
 import models.vgg16 as vgg16
 import models.vgg_utils as vgg_utils
 from skimage.transform import resize
@@ -38,7 +38,7 @@ def guided_BP(image, label_id = -1):
 
 			#creating the output vector for the respective class
 			index = np.argmax(prob)
-			print "Predicted_class: ", index
+			print("Predicted_class: {}".format(index))
 			output[index] = 1.0
 
 		else:
@@ -48,105 +48,93 @@ def guided_BP(image, label_id = -1):
 
 	return gb_grad_value[0] 
 
+def calc_derivatives(cost, label_index, target_conv_layer_grad):
+    first_derivative = tf.exp(cost)[0][label_index]*target_conv_layer_grad 	
+    second_derivative = tf.exp(cost)[0][label_index]*target_conv_layer_grad*target_conv_layer_grad 
+    triple_derivative = tf.exp(cost)[0][label_index]*target_conv_layer_grad*target_conv_layer_grad*target_conv_layer_grad
+    return first_derivative, second_derivative, triple_derivative
+
 def grad_CAM_plus(filename, label_id, output_filename):
-	g = tf.get_default_graph()
-	init = tf.global_variables_initializer()
+    g = tf.get_default_graph()
+    init = tf.global_variables_initializer()
+    
+    # Run tensorflow 
+    sess = tf.Session()
+
+    #define your tensor placeholders for, labels and images
+    label_vector = tf.placeholder("float", [None, 1000])
+    input_image = tf.placeholder("float", [1, 224, 224, 3])
+    label_index = tf.placeholder("int64", ())
+
+    #load vgg16 model
+    vgg = vgg16.Vgg16()
+    with tf.name_scope("content_vgg"):
+        vgg.build(input_image)
+    #prob = tf.placeholder("float", [None, 1000])
+
+    #get the output neuron corresponding to the class of interest (label_id)
+    cost = vgg.fc8*label_vector
+
+    # Get last convolutional layer gradients for generating gradCAM++ visualization
+    target_conv_layer = vgg.conv5_3
+    target_conv_layer_grad = tf.gradients(cost, target_conv_layer)[0]
+    '''
+    first_derivative, second_derivative, triple_derivative = \
+            calc_derivatives(cost, label_index, target_conv_layer_grad)
+    '''
+    #first_derivative
+    first_derivative = tf.exp(cost)[0][label_index]*target_conv_layer_grad 	
 	
-	# Run tensorflow 
-	sess = tf.Session()
+    #second_derivative
+    second_derivative = tf.exp(cost)[0][label_index]*target_conv_layer_grad*target_conv_layer_grad 
 
-	#define your tensor placeholders for, labels and images
-	label_vector = tf.placeholder("float", [None, 1000])
-	input_image = tf.placeholder("float", [1, 224, 224, 3])
-	label_index = tf.placeholder("int64", ())
+    #triple_derivative
+    triple_derivative = tf.exp(cost)[0][label_index]*target_conv_layer_grad*target_conv_layer_grad*target_conv_layer_grad  
 
-	#load vgg16 model
-	vgg = vgg16.Vgg16()
-	with tf.name_scope("content_vgg"):
-	    vgg.build(input_image)
-	#prob = tf.placeholder("float", [None, 1000])
-
-	#get the output neuron corresponding to the class of interest (label_id)
-	cost = vgg.fc8*label_vector
-
-	# Get last convolutional layer gradients for generating gradCAM++ visualization
-	target_conv_layer = vgg.conv5_3
-	target_conv_layer_grad = tf.gradients(cost, target_conv_layer)[0]
-	
-	#first_derivative
-	first_derivative = tf.exp(cost)[0][label_index]*target_conv_layer_grad 	
-	
-	#second_derivative
-	second_derivative = tf.exp(cost)[0][label_index]*target_conv_layer_grad*target_conv_layer_grad 
-
-	#triple_derivative
-	triple_derivative = tf.exp(cost)[0][label_index]*target_conv_layer_grad*target_conv_layer_grad*target_conv_layer_grad  
-
-	sess.run(init)
-
-	img1 = vgg_utils.load_image(filename)
-		
-	output = [0.0]*vgg.prob.get_shape().as_list()[1] #one-hot embedding for desired class activations
-		#creating the output vector for the respective class
-	
-	if label_id == -1:
-		prob_val = sess.run(vgg.prob, feed_dict={input_image:[img1]})
-		vgg_utils.print_prob(prob_val[0], './synset.txt')
-		#creating the output vector for the respective class
-		index = np.argmax(prob_val)
-		orig_score = prob_val[0][index]
-		print "Predicted_class: ", index
-		output[index] = 1.0
-		label_id = index
-	else:
-		output[label_id] = 1.0	
-	output = np.array(output)
-	print label_id
-	conv_output, conv_first_grad, conv_second_grad, conv_third_grad = sess.run([target_conv_layer, first_derivative, second_derivative, triple_derivative], feed_dict={input_image:[img1], label_index:label_id, label_vector: output.reshape((1,-1))})
-	
-	global_sum = np.sum(conv_output[0].reshape((-1,conv_first_grad[0].shape[2])), axis=0)
-
-	alpha_num = conv_second_grad[0]
-	alpha_denom = conv_second_grad[0]*2.0 + conv_third_grad[0]*global_sum.reshape((1,1,conv_first_grad[0].shape[2]))
-	alpha_denom = np.where(alpha_denom != 0.0, alpha_denom, np.ones(alpha_denom.shape))
-	alphas = alpha_num/alpha_denom
-
-	weights = np.maximum(conv_first_grad[0], 0.0)
-	#normalizing the alphas
-	"""	
-	alpha_normalization_constant = np.sum(np.sum(alphas, axis=0),axis=0)
-	
-	alphas /= alpha_normalization_constant.reshape((1,1,conv_first_grad[0].shape[2]))
-	"""
-
-	alphas_thresholding = np.where(weights, alphas, 0.0)
-
-        alpha_normalization_constant = np.sum(np.sum(alphas_thresholding, axis=0),axis=0)
-        alpha_normalization_constant_processed = np.where(alpha_normalization_constant != 0.0, alpha_normalization_constant, np.ones(alpha_normalization_constant.shape))
-
-
-        alphas /= alpha_normalization_constant_processed.reshape((1,1,conv_first_grad[0].shape[2]))
-
-
-	
-	deep_linearization_weights = np.sum((weights*alphas).reshape((-1,conv_first_grad[0].shape[2])),axis=0)
-	#print deep_linearization_weights
-	grad_CAM_map = np.sum(deep_linearization_weights*conv_output[0], axis=2)
-
-	# Passing through ReLU
-	cam = np.maximum(grad_CAM_map, 0)
-	cam = cam / np.max(cam) # scale 0 to 1.0   
-
-	cam = resize(cam, (224,224))
-	# Passing through ReLU
-	cam = np.maximum(grad_CAM_map, 0)
-	cam = cam / np.max(cam) # scale 0 to 1.0    
-	cam = resize(cam, (224,224))
-
-
-	gb = guided_BP([img1], label_id)
-	visualize(img1, cam, output_filename, gb) 
-	return cam
+    sess.run(init)
+    img1 = vgg_utils.load_image(filename)
+    output = [0.0]*vgg.prob.get_shape().as_list()[1] #one-hot embedding for desired class activations
+    #creating the output vector for the respective class
+    if label_id == -1:
+        prob_val = sess.run(vgg.prob, feed_dict={input_image:[img1]})
+        vgg_utils.print_prob(prob_val[0], './synset.txt')
+        #creating the output vector for the respective class
+        index = np.argmax(prob_val)
+        orig_score = prob_val[0][index]
+        print("Predicted_class: {}".format(index))
+        output[index] = 1.0
+        label_id = index
+        output = np.array(output)
+    else:
+        output[label_id] = 1.0	
+        output = np.array(output)
+        print(label_id)
+    conv_output, conv_first_grad, conv_second_grad, conv_third_grad = sess.run([target_conv_layer, first_derivative, second_derivative, triple_derivative], feed_dict={input_image:[img1], label_index:label_id, label_vector: output.reshape((1,-1))})
+    global_sum = np.sum(conv_output[0].reshape((-1,conv_first_grad[0].shape[2])), axis=0)
+    alpha_num = conv_second_grad[0]
+    alpha_denom = conv_second_grad[0]*2.0 + conv_third_grad[0]*global_sum.reshape((1,1,conv_first_grad[0].shape[2]))
+    alpha_denom = np.where(alpha_denom != 0.0, alpha_denom, np.ones(alpha_denom.shape))
+    alphas = alpha_num/alpha_denom
+    weights = np.maximum(conv_first_grad[0], 0.0)
+    #normalizing the alphas
+    alphas_thresholding = np.where(weights, alphas, 0.0)
+    alpha_normalization_constant = np.sum(np.sum(alphas_thresholding, axis=0),axis=0)
+    alpha_normalization_constant_processed = np.where(alpha_normalization_constant != 0.0, alpha_normalization_constant, np.ones(alpha_normalization_constant.shape))
+    alphas /= alpha_normalization_constant_processed.reshape((1,1,conv_first_grad[0].shape[2]))
+    deep_linearization_weights = np.sum((weights*alphas).reshape((-1,conv_first_grad[0].shape[2])),axis=0)
+    #print deep_linearization_weights
+    grad_CAM_map = np.sum(deep_linearization_weights*conv_output[0], axis=2)
+    # Passing through ReLU
+    cam = np.maximum(grad_CAM_map, 0)
+    cam = cam / np.max(cam) # scale 0 to 1.0   
+    cam = resize(cam, (224,224))
+    # Passing through ReLU
+    cam = np.maximum(grad_CAM_map, 0)
+    cam = cam / np.max(cam) # scale 0 to 1.0    
+    cam = resize(cam, (224,224))
+    gb = guided_BP([img1], label_id)
+    visualize(img1, cam, output_filename, gb) 
+    return cam
 
 def visualize(img, cam, filename,gb_viz):
     gb_viz = np.dstack((
