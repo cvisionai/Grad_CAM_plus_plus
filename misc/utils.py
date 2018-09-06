3fb25aa23296d28c40516f8071b550b18803ef39
Benjamin Woodward <benjamin.woodward@cvisionconsulting.com>
2018-09-06 08:50:33 -0400

    clean up code

--- misc/utils.py
+++ misc/utils.py
@@ -2,6 +2,7 @@ from PIL import Image
 import tensorflow as tf
 import numpy as np
 import keras.backend as K
+import pickle
 from keras.layers.core import Lambda
 from keras.models import Sequential
 from keras.models import model_from_yaml
@@ -72,6 +73,8 @@ def calc_derivatives(cost, label_index, target_conv_layer_grad):
 
 def prep_image_for_model(filename, mean_img, size=(224,224)):
     img = np.array(Image.open(filename).resize(size, Image.BICUBIC),dtype='float')
+    if img.shape[2] == 4:
+        img = img[:,:,0:3]
     img = img - np.array(Image.open(mean_img),dtype='float')
     return img
 
@@ -80,65 +83,43 @@ def grad_CAM_plus(filename, label_id, output_filename):
     init = tf.global_variables_initializer()
     
     # Run tensorflow 
-    arch_path = os.path.join('/mnt/md0/Projects/FathomNet/Training_Files/Midwater/snapshots', "cnn_architecture.yaml")
+    arch_path = os.path.join('/mnt/md0/Projects/FathomNet/Training_Files/Benthic/snapshots', "cnn_architecture.yaml")
     model_f = open(arch_path, "r")
     model = model_from_yaml(model_f.read())
-    epoch = 35
-    nb_classes = 15
+    epoch = 29
+    nb_classes = 12
     sess = tf.Session()
     K.set_session(sess)
     #define your tensor placeholders for, labels and images
     label_vector = tf.placeholder("float", [None, nb_classes])
-    input_image = tf.placeholder("float", [1, 224, 224, 3])
-
-    #load vgg16 model
-    #vgg = vgg16.Vgg16()
-    #with tf.name_scope("content_vgg"):
-    #    vgg.build(input_image)
-    #prob = tf.placeholder("float", [None, 1000])
-
-    #get the output neuron corresponding to the class of interest (label_id)
-    #cost = vgg.fc8*label_vector
-
+    input_image = tf.placeholder("float", [None, 224, 224, 3])
     label_index = tf.placeholder("int64", ())
-    input_img = prep_image_for_model(filename,'/mnt/md0/Projects/FathomNet/Training_Files/Midwater/snapshots/mean_img.png')
-    #output = model.predict(np.expand_dims(input_img,axis=0))
-    #max_class = np.argmax(output)
-    #output = [1 if i==max_class else 0 for i,elem in enumerate(output)]
-
-    #model = Model(inputs=model.input,
-    #              outputs=model.layers[-2].output)
-    #cost = model.predict(np.expand_dims(input_img,axis=0))
-    #Make this a one hot vector of cost for desired class
-    output = np.zeros((nb_classes,1))
-    output[label_id] = 1
-    print([layer.name for layer in model.layers if layer.name == 'activation_49'])
-    cost = model.layers[-1].output * label_vector
-    target_conv_layer = model.get_layer('activation_49').output
-    
+    input_img = prep_image_for_model(filename,'/mnt/md0/Projects/FathomNet/Training_Files/Benthic/snapshots/mean_img.png')
+    cost = model.layers[-2].output * label_vector
+    target_conv_layer = model.get_layer('activation_46').output
     # Get last convolutional layer gradients for generating gradCAM++ visualization
     target_conv_layer_grad = tf.gradients(cost, target_conv_layer)[0]
 
     first_derivative, second_derivative, triple_derivative = \
             calc_derivatives(cost, label_index, target_conv_layer_grad)
-    
     sess.run(init)
-    weights_path = os.path.join('/mnt/md0/Projects/FathomNet/Training_Files/Midwater/snapshots',
+    weights_path = os.path.join('/mnt/md0/Projects/FathomNet/Training_Files/Benthic/snapshots',
         "cnn_weights_epoch{:02d}.h5".format(epoch))
     model.load_weights(weights_path)
     #img1 = vgg_utils.load_image(filename)
     #output = [0.0]*vgg.prob.get_shape().as_list()[1] #one-hot embedding for desired class activations
     #creating the output vector for the respective class
     prob_val = model.predict(np.expand_dims(input_img,axis=0))
+    output = np.zeros((nb_classes,1))
     if label_id == -1:
-        prob_val = sess.run(vgg.prob, feed_dict={input_image:[img1]})
-        vgg_utils.print_prob(prob_val[0], './synset.txt')
         #creating the output vector for the respective class
         index = np.argmax(prob_val)
         orig_score = prob_val[0][index]
         print("Predicted_class: {}".format(index))
         output[index] = 1.0
         label_id = index
+        with open("output/" + output_filename.split(".")[0] + ".txt", "w") as text_file:
+            text_file.write(f'Label ID: {label_id}')
         output = np.array(output)
     else:
         output[label_id] = 1.0	
@@ -161,70 +142,41 @@ def grad_CAM_plus(filename, label_id, output_filename):
     grad_CAM_map = np.sum(deep_linearization_weights*conv_output[0], axis=2)
     # Passing through ReLU
     cam = np.maximum(grad_CAM_map, 0)
-    cam = cam / np.max(cam) # scale 0 to 1.0   
-    cam = resize(cam, (224,224))
-    # Passing through ReLU
-    cam = np.maximum(grad_CAM_map, 0)
-    cam = cam / np.max(cam) # scale 0 to 1.0    
-    cam = resize(cam, (224,224))
-    #gb = guided_BP([img1], label_id)
-    img1 = np.array(Image.open(filename).resize((224,224), Image.BICUBIC),dtype='uint8')
+    cam = cam / np.max(cam) # scale 0 to 1.0 
+    # Upsample with proper localization
+    padded_cam = np.zeros((15,15))
+    padded_cam[4:11,4:11] = cam
+    padded_cam = resize(padded_cam,(32*15,32*15),order=3)
+    cam = padded_cam[128:128+224,136:136+224]
+    #cam = resize(cam,(224,224),order=3)
+    # Original image
+    img1 = np.array(Image.open(filename),dtype='uint8')
+    if img1.shape[2] == 4:
+        img1 = img1[:,:,0:3]
+    #Resize cam to original image size
+    cam = resize(cam, (img1.shape[0],img1.shape[1]),order=3)
     visualize(img1, cam, output_filename) 
     return cam
 
 def visualize(img, cam, filename):
-    '''
-    gb_viz = np.dstack((
-            gb_viz[:, :, 2],
-            gb_viz[:, :, 1],
-            gb_viz[:, :, 0],
-        ))
-
-    gb_viz -= np.min(gb_viz)
-    gb_viz /= gb_viz.max()
-    '''
     fig, ax = plt.subplots(nrows=1,ncols=3)
 
-    plt.subplot(141)
+    plt.subplot(131)
     plt.axis("off")
     imgplot = plt.imshow(img)
 
-    plt.subplot(142)
-    '''
-    gd_img = gb_viz*np.minimum(0.25,cam).reshape(224,224,1)
-    x = gd_img
-    x = np.squeeze(x)
-    
-    #normalize tensor
-    x -= x.mean()
-    x /= (x.std() + 1e-5)
-    x *= 0.1
-
-    # clip to [0, 1]
-    x += 0.5
-    x = np.clip(x, 0, 1)
-
-    # convert to RGB array
-    x *= 255
-   
-    x = np.clip(x, 0, 255).astype('uint8')
-    '''
-    plt.axis("off")
-    #imgplot = plt.imshow(x, vmin = 0, vmax = 20)
-
     cam = (cam*-1.0) + 1.0
-    cam_heatmap = np.array(cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET))
-    plt.subplot(143)
+    cam_heatmap = np.roll(np.array(cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_JET)),0,axis=1)
+    pickle.dump(cam_heatmap,open("output/" + filename.split(".")[0] + ".pkl",'wb'))
+    plt.subplot(132)
     plt.axis("off")
 
-    imgplot = plt.imshow(cam_heatmap)
+    imgplot = plt.imshow(cam_heatmap,interpolation='None')
 
-    plt.subplot(144)
+    plt.subplot(133)
     plt.axis("off")
     
-    cam_heatmap = cam_heatmap/255.0
-
-    fin = (img*0.7) + (cam_heatmap*0.3)
+    fin = np.array((img*0.7) + (cam_heatmap*0.3),dtype='uint8')
     imgplot = plt.imshow(fin)
 
     plt.savefig("output/" + filename, dpi=600)
