71e44d306f9509fc1b9e11bd3dcdc402434199d5
Benjamin Woodward <benjamin.woodward@cvisionconsulting.com>
2018-09-07 09:16:19 -0400

    Code clean up and commenting

--- classify_batch.py
+++ classify_batch.py
@@ -11,10 +11,10 @@ import os
 import time
 
 def run(filename_list, class_label, out_dir):
-    arch_path = '/mnt/md0/Projects/FathomNet/Training_Files/Midwater/snapshots/'
-    epoch = 35
-    nb_classes = 15
-    mean_img_path = '/mnt/md0/Projects/FathomNet/Training_Files/Midwater/mean_img.png'
+    arch_path = '/mnt/md0/Projects/FathomNet/Training_Files/Benthic/snapshots/'
+    epoch = 33
+    nb_classes = 12
+    mean_img_path = '/mnt/md0/Projects/FathomNet/Training_Files/Benthic/mean_image.png'
     activation_layer = 'activation_46'
     utils.grad_CAM_plus_batch(filename_list, class_label, arch_path, epoch, nb_classes, out_dir, mean_img_path, activation_layer='activation_46')
 
@@ -24,7 +24,7 @@ def main():
     parser.add_argument('-gpu', '--gpu_device', default="0", type=str, help='if 0 (default) choose gpu 0, else user supplied int')
     parser.add_argument('-d', '--dir_name', type=str, help="Specify image directory for Grad-CAM++ visualization")
     parser.add_argument('-o', '--out_dir', type=str, help="Output directory to write to")
-    parser.add_argument('-b', '--batch_size', default=128, type=int, help="Number of images to process in a batch")
+    parser.add_argument('-b', '--batch_size', default=256, type=int, help="Number of images to process in a batch")
     args = parser.parse_args()
     gpu_id = args.gpu_device
     os.environ["CUDA_VISIBLE_DEVICES"]=gpu_id
